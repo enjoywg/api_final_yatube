2b43192b715ff3f9ec1c09af2fd527ff020c6d01
enjoyw <enjoywg@gmail.com>
2022-06-03 10:57:25 +0300

    Sprint 9

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# api_final
-api final
+# API Yatube
+API для сайта yatube
+
+Доступны для работы через API:
+- посты
+- группы
+- комментарии
+- подписки
+- токены
+
+## Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone https://github.com/enjoywg/api_final_yatube
+```
+```
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+```
+python3 -m venv venv
+```
+```
+source env/bin/activate
+```
+
+Установить зависимости из файла requirements.txt:
+```
+python3 -m pip install --upgrade pip
+```
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+```
+python3 manage.py migrate
+```
+
+Запустить проект:
+```
+python3 manage.py runserver
+```
+
+## Примеры запросов к API
+### Получение публикаций
+```
+GET /api/v1/posts/
+```
+
+### Создание публикации
+```
+POST /api/v1/posts/
+```
+Request Body schema
+```
+{
+    text (required): string (текст публикации)
+    image: string or null <binary>
+    group: integer or null (id сообщества)
+}
+```
+
+### Подписки
+```
+GET /api/v1/follow/
+```
+
+### Получение комментариев
+```
+GET /api/v1/posts/{post_id}/comments/
+```
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Follow, Group, Post
 
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -14,10 +17,42 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = SlugRelatedField(read_only=True, slug_field='username')
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = SlugRelatedField(queryset=User.objects.all(),
+                                 slug_field='username')
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='You are already followed'
+            )
+        ]
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError(
+                'You cannot follow yourself')
+        return data
